65bbfcac80e0b5309e18419dbc43e2826bf47ef5
Alex <alexsantos2102@gmail.com>
2018-03-15 02:14:29 -0300

    chamada firebase

---
+++ app/src/main/java/com/br/virtualstore/firebase/MyFirebaseInstanceIDService.java
@@ -0,0 +1,10 @@
+package com.br.virtualstore.firebase;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Alex on 05/03/2018.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/com/br/virtualstore/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package com.br.virtualstore.firebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by Alex on 05/03/2018.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
